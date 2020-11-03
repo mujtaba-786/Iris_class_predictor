360b4493da5f1f1360e4f97b68db0ef736f1a3d1
MOHD MUJTABA <56441628+mujtaba-786@users.noreply.github.com>
2020-11-04 01:53:47 +0530

    Update iris_webapp.py

--- iris_webapp.py
+++ iris_webapp.py
@@ -15,21 +15,19 @@ def classify(num):
     else:
         return 'Virginica'
 def main():
-    st.title("Streamlit Tutorial")
-    html_temp = """
-    <div style="background-color:teal ;padding:10px">
-    <h2 style="color:white;text-align:center;">Iris Classification</h2>
-    </div>
-    """
-    st.markdown(html_temp, unsafe_allow_html=True)
-    activities=['Linear Regression','Logistic Regression','SVM']
-    option=st.sidebar.selectbox('Which model would you like to use?',activities)
-    st.subheader(option)
-    sl=st.slider('Select Sepal Length', 0.0, 10.0)
-    sw=st.slider('Select Sepal Width', 0.0, 10.0)
-    pl=st.slider('Select Petal Length', 0.0, 10.0)
-    pw=st.slider('Select Petal Width', 0.0, 10.0)
-    inputs=[[sl,sw,pl,pw]]
+    img=Image.open('iris.jpg')
+    slt.sidebar.image(img)
+
+
+    slt.title('IRIS CLASS PREDICTOR')
+    slt.sidebar.info('Below is the link for wikipedia of iris')
+    slt.sidebar.success('https://en.wikipedia.org/wiki/Iris_(plant)')
+
+    sepal_length=slt.number_input('LENGTH OF SEAL',  min_value=0.0, max_value=20.0,value=4.0, step=0.1)
+    sepal_width=slt.number_input('WIDTH OF SEPAL', min_value=0.0, max_value=20.0,value=4.0, step=0.1)
+    petal_length=slt.number_input('LENGTH OF PETAL', min_value=0.0, max_value=20.0,value=4.0, step=0.1)
+    petal_width=slt.number_input('WIDTH OF PETAL', min_value=0.0, max_value=20.0,value=4.0, step=0.1)
+    inputs=[[sepal_length,sepal_width,petal_length,petal_width]]
     if st.button('Classify'):
         if option=='Linear Regression':
             st.success(classify(lin_model.predict(inputs)))
