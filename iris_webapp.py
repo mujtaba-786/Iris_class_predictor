702897d391100d63cd2cc293feea987d301e67d3
MOHD MUJTABA <56441628+mujtaba-786@users.noreply.github.com>
2020-12-01 18:22:25 +0530

    Update iris_webapp.py

--- iris_webapp.py
+++ iris_webapp.py
@@ -16,7 +16,7 @@ def classify(num):
     else:
         return 'Virginica'
 def main():
-    slt.markdown('<style>body{background-color: Green;}</style>',unsafe_allow_html=True)
+#     slt.markdown('<style>body{background-color: Green;}</style>',unsafe_allow_html=True)
     img=Image.open('irisImage.jpg')
     slt.sidebar.image(img)
 
